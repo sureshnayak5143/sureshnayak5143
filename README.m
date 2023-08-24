628b4ba4558194629ec71f573fbb85c849721a46
sureshnayak5143 <143091433+sureshnayak5143@users.noreply.github.com>
2023-08-24 17:44:05 +0530

    Suresh nayak
    Suoer

---
+++ README.m
@@ -0,0 +1,13 @@
+- 👋 H I’m @sureshnayak5143
+- 👀 I’m interested in ...
+- 🌱 I’m currently learning ...
+- 💞️ I’m looking to collaborate on ...
+- 📫 How to reach me ...
+
+<!---
+sureshnayak5143/sureshnayak5143 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
+suresh
+18192210000117bs (password)
+
